4cbd152588c490cc1303fbb2381a4f99ab88e9c5
athatheo <athatheoc@gmail.com>
2020-05-09 01:42:08 +0300

    Ah yes, this works :)

--- SOM.py
+++ SOM.py
@@ -9,7 +9,8 @@ def load_data(choice):
     if choice == 1:
         dataset = load_iris()
         data = pd.DataFrame(dataset['data'], columns=['Petal length', 'Petal Width', "Sepal Length", "Sepal Width"])
-        return data
+        classes = pd.DataFrame(dataset['target'])
+        return data, classes
 
 
 def create_2d_map(side_len, input_dimensions):
@@ -19,19 +20,18 @@ def create_2d_map(side_len, input_dimensions):
 
 
 def fit(epochs, map, data):
-    prev_MAP = np.zeros((5,5,4))
+    prev_MAP = np.zeros((len(map),len(map[0]),4))
     convergence = [1]
     for epoch in range(epochs):
+        print(epoch)
         # Randomize the order of the data
         data = data.sample(frac=1).reset_index(drop=True)
-
         learning_rate = update_learning_rate(epoch, epochs)
         radius = update_radius(epoch, epochs)
         neighborhood = update_neighborhood(learning_rate)
 
         for index, row in data.iterrows():
-            vector = [row[0], row[1], row[2], row[3]]
-            map = update(vector, map, learning_rate, radius, neighborhood)
+            map = update(row, map, learning_rate, radius, neighborhood)
 
         J = np.linalg.norm(map - prev_MAP)
 
@@ -45,12 +45,13 @@ def fit(epochs, map, data):
 
 
 def find_minimum(vector, map):
-    min = float('-inf')
+    min = float('inf')
     x, y = 0, 0
     for i in range(len(map)):
         for j in range(len(map[0])):
-            if np.linalg.norm(vector - map[i][j]) < min:
-                min = euclid_distance(vector, map[i][j])
+            distance = euclid_distance(vector, map[i][j])
+            if distance < min:
+                min = distance
                 x, y = i, j
     return x, y
 
@@ -64,24 +65,25 @@ def update(vector, map, learning_rate, radius, neighborhood):
     bmu = map[x][y]
     for i in range(len(map)):
         for j in range(len(map[0])):
-            if euclid_distance(map[i][j], bmu) < radius:
+            if euclid_distance(np.asarray([i,j]), np.asarray([x,y])) < radius:
                 map[i][j] = map[i][j] + learning_rate * neighborhood * (vector - map[i][j])
     return map
 
 
 def update_radius(epoch, epochs):
-    radius = 3
+    radius = 5
     return radius * math.exp(-epoch / epochs)
 
 
 def update_neighborhood(learning_rate):
+    neighborhood = 1
     # Bubble neighborhood function
-    neighborhood = learning_rate
+    #neighborhood = learning_rate
     return neighborhood
 
 
 def update_learning_rate(epoch, epochs):
-    learning_rate = 0.9
+    learning_rate = 0.25
     factor = 1 - (epoch / epochs)
     # factor = 1/iteration
     # factor = math.exp(iteration/epochs)
@@ -95,24 +97,45 @@ def euclid_distance(input_vector, unit):
 
 # Get the data
 # choice = int(input("Choose dataset: \n - For Iris dataset, type 1 \n"))
-df = load_data(1)  ##### REMEMBER TO CHANGE THIS TO CHOICE
-
+df, classes = load_data(1)  ##### REMEMBER TO CHANGE THIS TO CHOICE
+classes = classes.to_numpy()
 # Initialize the model
-map = create_2d_map(5, len(df.columns))
-
+map = create_2d_map(6, len(df.columns))
 # Train it
-map, convergence, J = fit(epochs=500, map=map, data=df)
+map, convergence, J = fit(epochs=200, map=map, data=df)
 # Print results
-from multiprocessing import Process
-
-#p = Process(target=fit, args=(500, map, df,))
-#p.start()
-#p.join()
-
+'''
 plt.plot(convergence)
 plt.ylabel('error')
 plt.xlabel('epoch')
 plt.grid(True)
 plt.yscale('log')
-plt.show()
+#plt.show()
 print('Final error: ' + str(J))
+'''
+result_map = np.zeros([len(map), len(map[0]), 3], dtype=np.float32)
+plt.clf()
+i = 0
+for index, row in df.iterrows():
+    BMU = find_minimum(row, map)
+    x = BMU[0]
+    y = BMU[1]
+    if classes[i] == 0:
+        if result_map[x][y][0] <= 0.5:
+            result_map[x][y] += np.asarray([0.5, 0, 0])
+    elif classes[i] == 1:
+        if result_map[x][y][1] <= 0.5:
+            result_map[x][y] += np.asarray([0, 0.5, 0])
+    elif classes[i] == 2:
+        if result_map[x][y][2] <= 0.5:
+            result_map[x][y] += np.asarray([0, 0, 0.5])
+    i += 1
+print(result_map)
+
+print("Red = Iris-Setosa")
+print("Blue = Iris-Virginica")
+print("Green = Iris-Versicolor")
+
+#print(map)
+plt.imshow(result_map, interpolation='nearest')
+plt.show()
