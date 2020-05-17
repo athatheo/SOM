59a47d1be0cfbe82a3446e77d87ad3871eb64cf8
athatheo <athatheoc@gmail.com>
2020-05-17 13:20:26 +0300

    Improving functionality and readability

--- SOM.py
+++ SOM.py
@@ -1,16 +1,21 @@
+import itertools
 import math
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
+from sklearn.datasets import load_breast_cancer
 
 
 def load_data(choice):
     if choice == 1:
         dataset = load_iris()
-        data = pd.DataFrame(dataset['data'], columns=['Petal length', 'Petal Width', "Sepal Length", "Sepal Width"])
-        classes = pd.DataFrame(dataset['target'])
-        return data, classes
+        data = pd.DataFrame(dataset['data'], columns=['Petal length', 'Petal Width', "Sepal Length", "Sepal Width"]).to_numpy()
+        classes = np.squeeze(pd.DataFrame(dataset['target']).to_numpy())
+    elif choice == 2:
+        data, classes = load_breast_cancer(True)
+
+    return data, classes
 
 
 def create_2d_map(side_len, input_dimensions):
@@ -20,65 +25,57 @@ def create_2d_map(side_len, input_dimensions):
 
 
 def fit(epochs, map, data):
-    prev_MAP = np.zeros((len(map),len(map[0]),4))
-    convergence = [1]
+    prev = np.zeros((len(map), len(map[0]), len(map[0][0])))
+    temp_data = np.copy(data)
     for epoch in range(epochs):
-        print(epoch)
         # Randomize the order of the data
-        data = data.sample(frac=1).reset_index(drop=True)
+        np.random.shuffle(temp_data)
+
+        # Receive new parameters
         learning_rate = update_learning_rate(epoch, epochs)
-        radius = update_radius(epoch, epochs)
+        radius = update_radius(epoch, epochs, len(map))
         neighborhood = update_neighborhood(learning_rate)
 
-        for index, row in data.iterrows():
+        # Update the map for every input vector
+        for row in temp_data:
             map = update(row, map, learning_rate, radius, neighborhood)
 
-        J = np.linalg.norm(map - prev_MAP)
+        # Metric to keep track of the convergence
+        diff = euclid_distance(map, prev)
+        prev = np.copy(map)
 
-        prev_MAP = np.copy(map)
-
-        if J < min(convergence):
-            print('Lower error found: %s' % str(J) + ' at epoch: %s' % str(epoch))
-        convergence.append(J)
-
-    return map, convergence, J
+        print("Epoch:", epoch+1, "/", epochs, "Distance since previous map: ", diff)
+    return map
 
 
 def find_minimum(vector, map):
     min = float('inf')
-    x, y = 0, 0
-    for i in range(len(map)):
-        for j in range(len(map[0])):
-            distance = euclid_distance(vector, map[i][j])
-            if distance < min:
-                min = distance
-                x, y = i, j
-    return x, y
-
-
-def plot():
-    plt.show()
+    bmu_row, bmu_col = 0, 0
+    for row, col in itertools.product(range(len(map)), range(len(map[0]))):
+        distance = euclid_distance(vector, map[row][col])
+        if distance < min:
+            min = distance
+            bmu_row, bmu_col = row, col
+    return bmu_row, bmu_col
 
 
 def update(vector, map, learning_rate, radius, neighborhood):
-    x, y = find_minimum(vector, map)
-    bmu = map[x][y]
-    for i in range(len(map)):
-        for j in range(len(map[0])):
-            if euclid_distance(np.asarray([i,j]), np.asarray([x,y])) < radius:
-                map[i][j] = map[i][j] + learning_rate * neighborhood * (vector - map[i][j])
+    bmu_row, bmu_col = find_minimum(vector, map)
+    for row, col in itertools.product(range(len(map)), range(len(map[0]))):
+        if euclid_distance(np.asarray([row, col]), np.asarray([bmu_row, bmu_col])) < radius:
+            map[row][col] = map[row][col] + learning_rate * neighborhood * (vector - map[row][col])
     return map
 
 
-def update_radius(epoch, epochs):
-    radius = 5
+def update_radius(epoch, epochs, length_map):
+    radius = length_map/2
     return radius * math.exp(-epoch / epochs)
 
 
 def update_neighborhood(learning_rate):
     neighborhood = 1
     # Bubble neighborhood function
-    #neighborhood = learning_rate
+    # neighborhood = learning_rate
     return neighborhood
 
 
@@ -95,47 +92,32 @@ def euclid_distance(input_vector, unit):
     return np.linalg.norm(input_vector - unit)
 
 
+def plot(df, map, classes):
+    colored_map = np.zeros([len(map), len(map[0]), 3])
+    for i, row in zip(range(len(df)), df):
+        x, y = find_minimum(row, map)
+        if classes[i] == 0:
+            if colored_map[x][y][0] <=0.5:
+                colored_map[x][y] += np.asarray([0.5, 0, 0])
+        elif classes[i] == 1:
+            if colored_map[x][y][1] <=0.5:
+                colored_map[x][y] += np.asarray([0, 0.5, 0])
+        elif classes[i] == 2:
+            if colored_map[x][y][2] <=0.5:
+                colored_map[x][y] += np.asarray([0, 0, 0.5])
+    plt.imshow(colored_map)
+    plt.show()
+
+
 # Get the data
-# choice = int(input("Choose dataset: \n - For Iris dataset, type 1 \n"))
-df, classes = load_data(1)  ##### REMEMBER TO CHANGE THIS TO CHOICE
-classes = classes.to_numpy()
+choice = int(input("Choose dataset: \n - For Iris dataset, type 1 \n - For Breast Cancer dataset, type 2\n"))
+df, classes = load_data(choice)
 # Initialize the model
-map = create_2d_map(6, len(df.columns))
+map = create_2d_map(10, len(df[0]))
 # Train it
-map, convergence, J = fit(epochs=200, map=map, data=df)
+map = fit(epochs=500, map=map, data=df)
+
 # Print results
-'''
-plt.plot(convergence)
-plt.ylabel('error')
-plt.xlabel('epoch')
-plt.grid(True)
-plt.yscale('log')
-#plt.show()
-print('Final error: ' + str(J))
-'''
-result_map = np.zeros([len(map), len(map[0]), 3], dtype=np.float32)
-plt.clf()
-i = 0
-for index, row in df.iterrows():
-    BMU = find_minimum(row, map)
-    x = BMU[0]
-    y = BMU[1]
-    if classes[i] == 0:
-        if result_map[x][y][0] <= 0.5:
-            result_map[x][y] += np.asarray([0.5, 0, 0])
-    elif classes[i] == 1:
-        if result_map[x][y][1] <= 0.5:
-            result_map[x][y] += np.asarray([0, 0.5, 0])
-    elif classes[i] == 2:
-        if result_map[x][y][2] <= 0.5:
-            result_map[x][y] += np.asarray([0, 0, 0.5])
-    i += 1
-print(result_map)
+plot(df, map, classes)
 
-print("Red = Iris-Setosa")
-print("Blue = Iris-Virginica")
-print("Green = Iris-Versicolor")
 
-#print(map)
-plt.imshow(result_map, interpolation='nearest')
-plt.show()
